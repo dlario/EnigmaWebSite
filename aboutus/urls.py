28a8a868d3184965bc3e1f6934a8381e26318130
dlario <larioengineer@gmail.com>
2018-07-22 23:41:51 -0600

    The First Edition

--- aboutus/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('blog', views.blog, name='blog'),
-    path('contact', views.contact, name='contact'),
-    path('team', views.team, name='team'),
-]
--- inspections/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('create', views.create, name='create'),
-    path('detail', views.detail, name='detail'),
-    path('home', views.home, name='home'),
-]
