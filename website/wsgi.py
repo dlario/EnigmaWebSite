82f246ef90e59e46399d17c4c2ac5490c6052177
dlario <larioengineer@gmail.com>
2018-07-24 19:38:30 -0600

    updated the wsgi to website

--- website/wsgi.py
+++ website/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "InspectionProgram.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
 
 application = get_wsgi_application()
